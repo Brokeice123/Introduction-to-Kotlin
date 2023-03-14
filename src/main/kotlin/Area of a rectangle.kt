09549d549194ba1255ea1c9088e3ef56792136a7
Brokeice123 <keithmarkomen@gmail.com>
2023-03-14 12:13:47 +0300

    Did exercise of Kotlin by ding Area of Rectangle and BMI Calculator

---
+++ src/main/kotlin/Area of a rectangle.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var length = 30
+    var width = 40
+    var result = length * width
+    print(result)
+}
---
+++ src/main/kotlin/BMI Calculator.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var weight = 60
+    var height = 1.5
+    var square = height * height
+    var result = weight / square
+    print(result)
+}
