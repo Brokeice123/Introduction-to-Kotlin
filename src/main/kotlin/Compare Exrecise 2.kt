3327643531adb2b6bf2137fcb68e0d2951f5297d
Brokeice123 <keithmarkomen@gmail.com>
2023-03-15 12:27:26 +0300

    Comparison Exercise two

---
+++ src/main/kotlin/Compare Exrecise 2.kt
@@ -0,0 +1,57 @@
+fun main(args: Array<String>) {
+    print("Enter Option")
+    var one:Int = readLine()!!.toInt()
+        if (one == 0){
+            print("Sh20 1GB for 1hr")
+        }
+
+        if (one == 1){
+            print("Data Bundles")
+        }
+
+        if (one == 2){
+            print("Daily Bundles")
+        }
+
+        if (one == 3){
+            print("Weekly Bundles")
+        }
+
+        if (one == 4){
+            print("GO Monthly")
+        }
+
+        if (one == 5){
+            print("No Expiry")
+        }
+
+        if (one == 6){
+            print("Video Bundles")
+        }
+
+        if (one == 7){
+            print("Okoa Data")
+        }
+
+        if (one == 8){
+            print("Lipa Mdogo")
+        }
+
+        if (one == 9){
+            print("Data Plus NEW")
+        }
+
+        if (one == 10){
+            print("Hot Minutes")
+        }
+
+        if (one == 98){
+            print("MORE")
+        }
+
+        else{
+            print("Invalid Character")
+        }
+
+
+}
