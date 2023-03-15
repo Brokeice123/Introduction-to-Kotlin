e6deb4e7d21751962fc26eea91085ce18f1a65b9
Brokeice123 <keithmarkomen@gmail.com>
2023-03-15 11:40:53 +0300

    DataTypes in Kotlin

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    //Integer or Int
+    var age:Int = 20
+    var age = 30
+    //Strings
+    var name:String = "Muraya" //Double quotes are used
+    //Double
+    var temperature = 36.4
+    var temperature:Double = 36.4
+    //Boolean{It can either be true or false}
+    var Status = false
+    //Float
+    var point:Float = 8.5F //{The F or float means that the decimal is to one dp}
+    //Convert Double to a float
+    var marks = 46.587F //{The capital F converts the marks to a float/ one dp}
+
+}
