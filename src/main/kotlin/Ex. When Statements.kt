d2424359adebc2933d1bb2165649e132c5b454f9
Brokeice123 <keithmarkomen@gmail.com>
2023-03-21 12:02:06 +0300

    USSD When Statements

--- src/main/kotlin/Ex. When Statements.kt
+++ src/main/kotlin/Ex. When Statements.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
+ println("Enter option")
+    var option:Int = readln()!!.toInt()
 
+    when (option){
+        0-> println("Sh20=1GB for 1hr")
+        1-> println("Data Deals")
+        2-> println("Daily Bundles")
+        3-> println("Weekly Bundles")
+        4-> println("GO Monthly")
+        5-> println("No Expiry")
+        6-> println("Video Bundles")
+        7-> println("Okoa Bundles")
+        8-> println("Lipa Mdogo")
+        9-> println("Data Plus NEW")
+        10-> println("Hot Minutes")
+
+        else-> println("Invalid Option")
+    }
 }
